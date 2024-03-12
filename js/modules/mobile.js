53d7202e606a89810766a572abbd52d396e09d51
Ana Nogueira <acsnogueira95@gmail.com>
2024-03-12 20:40:15 -0300

    iniciando testimonials

--- js/modules/mobile.js
+++ js/modules/mobile.js
@@ -1,14 +1,26 @@
 export default function initMobile() {
-    const dressStyleItems = document.querySelector('.dress-style-items');
+  // FUNÇÃO PARA MUDAR AS IMAGENS DA CLASS DRESS STYLE
+
+  const dressStyleItems = document.querySelector(".dress-style-items");
+  const defaultDressItems = dressStyleItems.innerHTML;
+
+  function checkWidth() {
     const windowWidth = window.innerWidth;
+    return windowWidth;
+  }
 
-    if (windowWidth < 610) {
-        console.log('menor')
-        dressStyleItems.innerHTML=`<a class="casual" href=""><img src="./src/img/gym.png" alt=""></a>
-        <a class="formal" href=""><img src="./src/img/formal.png" alt=""></a>
-        <a class="party" href=""><img src="./src/img/party.png" alt=""></a>
-        <a class="gym" href=""><img src="./src/img/gym.png" alt=""></a>`;
+  function dressStyleToMobile(width) {
+    if (width < 610) {
+      console.log("menor");
+      dressStyleItems.innerHTML = `<a class="casual" href=""><img src="./src/img/mobileimgs/casual.png" alt=""></a>
+        <a class="formal" href=""><img src="./src/img/mobileimgs/formal.png" alt=""></a>
+        <a class="party" href=""><img src="./src/img/mobileimgs/party.png" alt=""></a>
+        <a class="gym" href=""><img src="./src/img/mobileimgs/gym.png" alt=""></a>`;
+    } else {
+      dressStyleItems.innerHTML = defaultDressItems;
     }
-    
-
+  }
+  window.addEventListener("resize", () => {
+    dressStyleToMobile(checkWidth());
+  });
 }
