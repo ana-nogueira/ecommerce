1dae18fe437488defdd5bcd825062c30a2ec518f
ana-nogueira <acsnogueira95@gmail.com>
2024-03-13 17:23:17 -0300

    Finalização do testimonials e inicialização do newsletter

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+    var swiper = new Swiper(".testimonials-content", {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      freeMode: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+
 import initDropDown from "./modules/header.js";
 import initMobile from "./modules/mobile.js";
 
